e45b18f348f488a68a827d7f39fdfe00da44b80a
adauren <frankcawperwood818@gmail.com>
2019-09-01 13:03:15 +0600

    return jsonwebtoken after user registered

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
@@ -27,6 +29,7 @@ router.post(
     const { name, email, password } = req.body;
 
     try {
+      // check user existing
       let user = await User.findOne({ email });
 
       if (user) {
@@ -41,11 +44,28 @@ router.post(
         password
       });
 
+      // hash password
       const salt = await bcrypt.genSalt(10);
-
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
+
+      // return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get(jwtSecret),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server Error");
