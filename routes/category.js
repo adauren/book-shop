c54b3fb2c36d706a804cb9070cd74044ec4054ef
adauren <frankcawperwood818@gmail.com>
2019-09-02 16:23:26 +0600

    create category route refactor

--- routes/category.js
+++ routes/category.js
@@ -6,15 +6,44 @@ const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const Category = require("../models/Category");
 
-router.post("/category/create", auth, admin, async (req, res) => {
-  const category = new Category(req.body);
-  try {
-    await category.save();
-    res.json({ category });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error");
+router.post(
+  "/category/create",
+  [
+    check("name", "Category name is required")
+      .not()
+      .isEmpty()
+  ],
+  auth,
+  admin,
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, date } = req.body;
+
+    try {
+      // check category existing
+      let category = await Category.findOne({ name });
+
+      if (category) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Category already exists" }] });
+      }
+
+      category = new Category({ name, date });
+
+      await category.save();
+
+      res.json(category);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
   }
-});
+);
 
 module.exports = router;
