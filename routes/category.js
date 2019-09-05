27cdf92a1c04006eea7b7af5c9ad8ad21022b1e5
adauren <frankcawperwood818@gmail.com>
2019-09-05 20:49:56 +0600

    update category, delete category and get all categories

--- routes/category.js
+++ routes/category.js
@@ -38,7 +38,7 @@ router.post(
 
       await category.save();
 
-      res.json(category);
+      return res.json(category);
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server Error");
@@ -56,7 +56,57 @@ router.get("/category/:id", async (req, res) => {
       });
     }
 
-    res.status(400).json(category);
+    return res.status(400).json(category);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.put("/category/:id", auth, admin, async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+
+    category.name = req.body.name;
+
+    await category.save();
+
+    return res.status(400).json(category);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.delete("/category/:id", auth, admin, async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+
+    await category.remove();
+
+    res.json({ msg: "Category deleted" });
+  } catch (error) {
+    console.error(error);
+    if (error.kind === "ObjectId") {
+      return res.json({
+        errors: [{ msg: "Category not found" }]
+      });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get("/categories", async (req, res) => {
+  try {
+    const categories = await Category.find();
+
+    if (!categories) {
+      return res.json({
+        errors: [{ msg: "There is no category" }]
+      });
+    }
+
+    return res.status(400).json(categories);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
