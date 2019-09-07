30d08b23b83abbaa060bae0470b059aeded99a7b
adauren <frankcawperwood818@gmail.com>
2019-09-08 04:59:52 +0600

    client side basic setup

---
+++ client/src/Routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SignIn from "./user/SignIn";
+import SignUp from "./user/SignUp";
+import Home from "./core/Home";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" exact component={SignIn} />
+        <Route path="/signup" exact component={SignUp} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
