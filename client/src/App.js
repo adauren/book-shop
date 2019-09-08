626fbec808a329675937b741cc3c4da668ed9a71
adauren <frankcawperwood818@gmail.com>
2019-09-08 07:56:33 +0600

    small refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SignIn from "./user/SignIn";
+import SignUp from "./user/SignUp";
+import Home from "./core/Home";
 
 const App = () => {
   return (
-    <div>
-      <h2>App Component</h2>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" exact component={SignIn} />
+        <Route path="/signup" exact component={SignUp} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
